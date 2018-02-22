c20fe391280b27de36651e881a0913ff265e82ee
ouakira <1638897848@qq.com>
2018-02-22 14:39:59 +0800

    pipeline

--- src/com.at.wangxu/pipelinedemo/main.go
+++ src/com.at.wangxu/pipelinedemo/main.go
@@ -3,9 +3,33 @@ package main
 import (
 	"learninggo/src/pipeline"
 	"fmt"
+	"os"
+	"bufio"
 )
 
 func main() {
+	const filename  = "Lagare.in"
+	const count  =50
+	file,err:=os.Create(filename)
+	if err!=nil {
+		panic(err)
+	}
+	defer file.Close()
+	p:=pipeline.RandomSource(count)
+	buffer:=bufio.NewWriter(file)
+	pipeline.WriteSink(buffer,p)
+	buffer.Flush()
+	file,err=os.Open(filename)
+	if err!=nil {
+		panic(err)
+	}
+	defer file.Close()
+	p=pipeline.ReaderSource(bufio.NewReader(file))
+	for v:=range p{
+		fmt.Println(v)
+	}
+}
+func MergeDemo(){
 	p :=
 		pipeline.Merge(pipeline.InMemSort(pipeline.ArraySource(1, 3, 5, 7, 9, 0)), pipeline.InMemSort(pipeline.ArraySource(2, 4, 6, 8, 10, 1)))
 	for v := range p {
--- src/pipeline/nodes.go
+++ src/pipeline/nodes.go
@@ -1,6 +1,11 @@
 package pipeline
 
-import "sort"
+import (
+	"sort"
+	"io"
+	"encoding/binary"
+	"math/rand"
+)
 
 func ArraySource(a ...int) <-chan int {
 	out := make(chan int)
@@ -44,6 +49,41 @@ func Merge(in1, in2 <-chan int) <-chan int {
 				out <- v2
 				v2, ok2 = <-in2
 			}
+		}
+		close(out)
+	}()
+	return out
+}
+func ReaderSource(reader io.Reader) <-chan int {
+	out := make(chan int)
+	go func() {
+		buffer := make([]byte, 8)
+		for {
+			n, err := reader.Read(buffer)
+			if n > 0 {
+				v := int(binary.BigEndian.Uint64(buffer))
+				out <- v
+			}
+			if err != nil {
+				break
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+func WriteSink(writer io.Writer, in <-chan int) {
+	for v := range in {
+		buffer := make([]byte, 8)
+		binary.BigEndian.PutUint64(buffer, uint64(v))
+		writer.Write(buffer)
+	}
+}
+func RandomSource(count int) <-chan int {
+	out:=make(chan int)
+	go func() {
+		for i:=0;i<count ;i++  {
+			out<-rand.Int()
 		}
 		close(out)
 	}()
